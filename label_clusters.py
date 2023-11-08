06924f31e4670046bafea4138040707f728b3275
Bialek <pbialek@dcscorp.com>
2023-11-08 18:43:02 -0500

    started cluster labeling

---
+++ label_clusters.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pandas as pd
+import openai
+import json
+from collections import defaultdict
+
+from langchain import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
+# https://medium.com/@kbdhunga/text-clustering-and-labeling-utilizing-openai-api-677271e0763c
+# https://pypi.org/project/keybert/
+
+openai.api_key = ''
+input_datapath = 'data/all_reviews_with_embeddings.json'
+
+
+with open(input_datapath,'r') as json_file:
+    data = json.load(json_file)
+
+cluster_reviews = defaultdict(list)
+
+for hospital in data['hospitals']:
+    for review in hospital['reviews']:
+        review_txt = review['text']
+        cluster_id = review.get('cluster_id', 0)
+        cluster_reviews[cluster_id].append(review_txt)
+
+
+
+def call_openai_api(messages):
+    return openai.ChatCompletion.create(
+        model='gpt-3.5-turbo-16k',
+        messages=messages,
+        max_tokens=4096,
+        temperature=1
+    )
+
+
+def summarize_text(transcript):
+    system_prompt = "I would like for you to assume the role of a Life Coach"
+    user_prompt = f"""Generate ten words summary of the text below.
+    Text: {transcript}
+    """
+
+    messages = [
+        {'role': 'system', 'content': system_prompt},
+        {'role': 'user', 'content': user_prompt}
+    ]
+
+    response = call_openai_api(messages)
+    summary = response['choices'][0]['message']['content']
+    return summary
+
+def get_prompt():
+    system_template = "You are a market analyst and know all reviews from Amazon. You're helping me write a concise topic from many reviews."
+    human_template = "Using the following reviews, write a topic title that summarizes them.\n\nREVIEWS:{reviews}\n\nTOPIC TITLE:"
+
+    return ChatPromptTemplate(
+        messages=[
+            SystemMessagePromptTemplate.from_template(system_template),
+            HumanMessagePromptTemplate.from_template(human_template),
+        ],
+        input_variables=["reviews"],
+    )
+
+for c in df1.Cluster.unique():
+    chain = LLMChain(
+        llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k"), prompt=get_prompt(), verbose=False
+    )
+    review_str = "\n".join(
+        [
+            f"{review['summary_openai']}\n"
+            for review in df1.query(f"Cluster == {c}").to_dict(orient="records")
+        ]
+    )
+    result = chain.run(
+        {
+            "reviews": review_str,
+        }
+    )
+    df1.loc[df1.Cluster == c, "review_topic"] = result
