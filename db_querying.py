629acb5218f5aa85ed97142829d394de10147b9c
billydeehan <61507349+billydeehan@users.noreply.github.com>
2023-11-16 21:36:04 -0600

    query update

--- db_querying.py
+++ db_querying.py
@@ -2,20 +2,25 @@ import json
 import os
 import openai
 from azure.cosmos import CosmosClient
-cosmos_url = 'https://reviews-cosmosdb.documents.azure.com:443/'
-cosmos_key = ''
-db_name = 'reviews-db'
-container_name = 'reviews-container'
 
-client = CosmosClient(cosmos_url,credential=cosmos_key)
-database = client.create_database_if_not_exists(id=db_name)
-container = database.create_container_if_not_exists(
-    id=container_name,
-    partition_key='/place_id',
-    offer_throughput=1000
-)
+
+def getContainer():
+    cosmos_url = 'https://reviews-cosmosdb.documents.azure.com:443/'
+    cosmos_key = ''
+    db_name = 'reviews-db'
+    container_name = 'reviews-container'
+
+    client = CosmosClient(cosmos_url,credential=cosmos_key)
+    database = client.create_database_if_not_exists(id=db_name)
+    container = database.create_container_if_not_exists(
+        id=container_name,
+        partition_key='/place_id',
+        offer_throughput=1000
+    )
+    return container
 
 def queryReviews(query='SELECT * FROM c OFFSET 0 LIMIT 10000'):
+    container = getContainer()
     results = list(container.query_items(
         query,
         enable_cross_partition_query=True
@@ -23,6 +28,7 @@ def queryReviews(query='SELECT * FROM c OFFSET 0 LIMIT 10000'):
     return results
 
 def addCluster(reviews, cluster_label = 1):
+    container = getContainer()
     for review in reviews:
         review['cluster'] = cluster_label
         container.upsert_item(body=review)
